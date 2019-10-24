02663d6770a169968117341aaed543e4deab279a
Gustavo Afonso <gugustavo137@gmail.com>
2019-10-24 15:17:37 -0300

    Adicionadas as estruturas das funções

--- Trabalho_1/fila_vetor_tamanho_fixo.c
+++ Trabalho_1/fila_vetor_tamanho_fixo.c
@@ -1,9 +1,39 @@
-//#include "fila_vetor_tamanho_fixo.h"
+#include "fila_vetor_tamanho_fixo.h"
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 
-void imprimeNomeEstrutura()
-{
-    printf("Fila de vetor foi chamada corretamente\n");
+void CriarFila(int tamanho_da_fila){
+
+int fila[tamanho_da_fila];
+printf("fila de tamanho %d criada\n", tamanho_da_fila);
+
 }
 
+void InserirFila(int numero_inserido){
+
+}
+
+void RemoverFila(){
+
+}
+
+void FilaCheia(){
+
+//fila vai estar cheia quando o tamanho for igual a capacidade
+
+}
+
+void FilaVazia()
+{
+
+}
+
+void TamanhoFila()
+{
+
+}
+
+void ImprimirFila()
+{
+
+}
