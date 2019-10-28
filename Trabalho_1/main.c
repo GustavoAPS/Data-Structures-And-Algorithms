20a2da07f9e648a315a36f3d6bdde9d14914aa16
Gustavo Afonso <gugustavo137@gmail.com>
2019-10-28 16:22:35 -0300

    Atualizacao da main

--- Trabalho_1/main.c
+++ Trabalho_1/main.c
@@ -1,58 +1,40 @@
 #include <stdio.h>
 #include<stdlib.h>
-//#include "fila_vetor_tamanho_fixo.h"
-#include "fila_circular_redimensionavel.h"
+
+#include "fila_vetor_tamanho_fixo.h"
+//#include "fila_circular_redimensionavel.h"
 //#include "fila_lista_encadeada_circular.h"
 
 int main(){
 
-  int opc, n;
-  printf("\n 1- Inserir n elementos na fila");
+  int opc = 0;
+  int n;
+
+  while(opc != 5){
+
+    printf("\n 1- Inserir n elementos na fila");
     printf("\n 2- Remover n elementos na fila.");
     printf("\n 3- Imprimir a fila.");
     printf("\n 4 - Reiniciar a fila.");
     printf("\n 5 - Sair");
     printf("\n OPÇÃO =  ");
+
     scanf("%d", &opc);
   
-do
-    {
+    if(opc == 1){
+      CriarFila();
+      //printf("Digite quantos elementos voce deseja inserir:\n");
+      //       scanf("%d", &n);
+      //       InserirFila(n);
+    }
 
-       switch (opc)
-       {
-         
-       case 1:
+    if(opc == 2){
+      //printf("Digite quantos elementos voce deseja remover:\n");
+      //scanf("%d", &n);
+      //RemoverFila(n);
+      //printf("Elemento removido: %d\n", n);     
+    }
 
-          //CriarFila();
-        printf("Digite quantos elementos voce deseja inserir:\n");
-                scanf("%d", &n);
-               InserirFila(n);
-         break;
-       
-       case 2:
-        
-         printf("Digite quantos elementos voce deseja remover:\n");
-          scanf("%d", &n);
-          RemoverFila(n);
-          //printf("Elemento removido: %d\n", n);  
-
-         break;
-       
-         case 3:
-           
-          
-          ImprimirFila(5);
-      
-         break;
-         
-         case 4:
-           
-         
-            break;
-         
-       }
-       }while(opc!=5);
-       
-  
-    return 0;
-       }
+  }
+  return 0;
+  }
