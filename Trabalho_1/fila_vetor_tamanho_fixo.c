ee8fdf1ebb7638169aa29a2b294b30b00197d835
dani_so_o <dannysoaresdeoliveira@gmail.com>
2019-10-28 17:53:18 -0300

    modo fila circular_redimensional com problemas

--- Trabalho_1/fila_vetor_tamanho_fixo.c
+++ Trabalho_1/fila_vetor_tamanho_fixo.c
@@ -2,29 +2,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *fila;
+int *fila_ptr;
+int tamanho_da_fila = 0;
 
-void CriarFila(int tamanho_da_fila){
+void CriarFila()
+{
+    printf("Qual o tamanho da fila?");
+    scanf("%d", &tamanho_da_fila);
 
-    int fila_local[tamanho_da_fila];
-    
-    fila = (char *) malloc(tamanho_da_fila);
+    fila_ptr = malloc(tamanho_da_fila * sizeof(int));
 
     printf("fila de tamanho %d criada\n", tamanho_da_fila);
-    
+    printf("fila de tamanho %x criada\n", fila_ptr);
 }
 
-void InserirFila(int numero_inserido){
+void InserirFila(int numero_inserido)
+{
+    if (tamanho_da_fila == 0)
+        CriarFila();
 
+    int elemento;
+    int elemento_inserido = 0;
+
+    printf("Qual o elemento para inserir?\n");
+    scanf("%d", &elemento);
+
+    for(int i = 0; i < tamanho_da_fila; i++)
+    {
+        if(fila_ptr[i] == 0)
+        {
+            fila_ptr[i] = elemento;
+            break;
+        }
+    }
 }
 
 void RemoverFila(){
 
+    int elemento;    
+    printf("Remover elemento de qual posicao?\n");
+    scanf("%d", &elemento);
+
+    if (elemento < tamanho_da_fila - 1)
+        fila_ptr[elemento] = 0;
+    
 }
 
 void FilaCheia(){
 
-//fila vai estar cheia quando o tamanho for igual a capacidade
+
 
 }
 
@@ -33,12 +59,17 @@ void FilaVazia()
 
 }
 
-void TamanhoFila()
+void ReiniciaFila()
 {
-
+    tamanho_da_fila = 0;
 }
 
 void ImprimirFila()
 {
+
+    for(int i = 0; i < tamanho_da_fila; i++)
+    {
+        printf("\nElemento na posicao %d = %d", i, fila_ptr[i] );
+    }
 
 }
