aaef8b73e8c82a6569ad9862d89fba83850bebdf
Gustavo Afonso <gugustavo137@gmail.com>
2019-10-31 21:22:00 -0300

    Adicionando fila lista encadesda circular

--- Trabalho_1/fila_lista_encadeada_circular.c
+++ Trabalho_1/fila_lista_encadeada_circular.c
@@ -1,26 +1,80 @@
 #include <stdio.h>
-
+#include<stdlib.h>
+#include "fila_lista_encadeada_circular.h"
+ 
+typedef struct Node
+ 
+{
+	int info;
+	struct Node *next;
+}node;
+ 
+node *front=NULL,*rear=NULL,*temp;
+ 
 
   void CriarFila(){
 
-
-}
+	node *newnode;
+	newnode=(node*)malloc(sizeof(node));
+	printf("\nEnter the node value : ");
+	scanf("%d",&newnode->info);
+	newnode->next=NULL;
+	if(rear==NULL)
+	front=rear=newnode;
+	else
+	{
+		rear->next=newnode;
+		rear=newnode;
+	}
+	
+	rear->next=front;
+  }
   void InserirFila(){
 
     }
     void RemoverFila(){
 
+	temp=front;
+	if(front==NULL)
+		printf("\nUnderflow :");
+	else
+	{
+		if(front==rear)
+		{
+			printf("\n%d",front->info);
+			front=rear=NULL;
+		}
+		else
+		{
+			printf("\n%d",front->info);
+			front=front->next;
+			rear->next=front;
+		}
+ 
+	temp->next=NULL;
+	free(temp);
+	}
     }
     void FilaCheia(){
       
 
     }
     void FilaVazia(){
-          fila -> NULL;
+
     }
     void TamanhoFila(){
 
     }
     void ImprimirFila(){
-      
+	temp=front;
+	if(front==NULL)
+		printf("\nEmpty");
+	else
+	{
+		printf("\n");
+		for(;temp!=rear;temp=temp->next)
+			printf("\n%d address=%u next=%u\t",temp->info,temp,temp->next);
+			printf("\n%d address=%u next=%u\t",temp->info,temp,temp->next);
+	}      
     }
+
