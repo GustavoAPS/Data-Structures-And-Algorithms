440fbfc53a2a1a57827468dc276361d857b0b715
Gustavo Afonso <gugustavo137@gmail.com>
2021-09-16 11:40:53 -0300

    Adding binary threes examples

---
+++ binary_threes/binary_three_example_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node
+{
+	int key_data;
+	struct Node* left;
+	struct Node* right;
+};
+
+int main()
+{
+//Create node
+//new_node = Node()
+
+//assign
+//new_left_node()
+
+//assign
+//new_right_node()
+
+//print()
+return 0;
+}
+
---
+++ binary_threes/binary_threes.md
@@ -0,0 +1,5 @@
+#Binary threes
+
+
+The binary tree combines the concepts of dynamic memory allocation and binary search in just one structure.
+
